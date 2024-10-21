44c3652b976809a0bbeaf2e1b0872d911b280acf
SyedAsadK <syedasad1610@gmail.com>
2024-10-21 18:51:38 +0530

    Sqlite update

--- bin/main.go
+++ bin/main.go
@@ -1,13 +1,84 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	ray "github.com/gen2brain/raylib-go/raylib"
+	_ "github.com/mattn/go-sqlite3"
+	"log"
+	"os"
 	textbox "raytodo"
 )
 
+var db *sql.DB
+
+func initDB() {
+	if _, err := os.Stat("notes.db"); os.IsNotExist(err) {
+		file, err := os.Create("notes.db")
+		if err != nil {
+			log.Fatal(err)
+		}
+		file.Close()
+		fmt.Println("notes.db file created")
+	}
+	var err error
+	db, err = sql.Open("sqlite3", "./notes.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	createTableSQL := `CREATE TABLE IF NOT EXISTS notes (
+		"id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,		
+		"content" TEXT
+	);`
+
+	_, err = db.Exec(createTableSQL)
+	if err != nil {
+		log.Fatalf("Error creating table: %v", err)
+	}
+	fmt.Println("Connected to sql database and ensured notes table exists")
+}
+func insertNote(stuff string) {
+	insertNoteSQL := `INSERT INTO notes (content) VALUES (?)`
+	_, err := db.Exec(insertNoteSQL, stuff)
+	if err != nil {
+		fmt.Println("Error inserting note: ", err)
+	}
+}
+func removeNote(stuff string) {
+	removeNoteSQL := `DELETE FROM notes WHERE (id) =(SELECT max(id) FROM notes)`
+	_, err := db.Exec(removeNoteSQL, stuff)
+	if err != nil {
+		fmt.Println("Error deleting note: ", err)
+	}
+}
+func fetchNotes() []string {
+	rows, err := db.Query("SELECT content FROM notes")
+	if err != nil {
+		fmt.Println("Error Fetching note: ", err)
+		return nil
+	}
+	defer rows.Close()
+	var notes []string
+	for rows.Next() {
+		var stuff string
+		err = rows.Scan(&stuff)
+		if err != nil {
+			fmt.Println("Error scanning row : ", err)
+			continue
+		}
+		notes = append(notes, stuff)
+	}
+	return notes
+
+}
+
 func main() {
+	initDB()
+	defer db.Close()
+
 	textbox1 := textbox.NewTextbox(100, 100, 600, 50, 32)
-	notes := make([]string, 0)
+	notes := fetchNotes()
 
 	ray.InitWindow(840, 600, "Quick-Task")
 	ray.SetExitKey(ray.KeyQ)
@@ -24,13 +95,14 @@ func main() {
 		ray.DrawText("Add", 745, 110, 20, ray.Black)
 		if ray.IsMouseButtonPressed(ray.MouseLeftButton) {
 			if ray.CheckCollisionPointRec(ray.GetMousePosition(), but) {
+				insertNote(textbox1.Text)
 				notes = append(notes, textbox1.Text)
 				textbox1.Text = ""
 			}
 		}
 		ray.DrawText("Quick Task List", 100, 50, 20, ray.LightGray)
 		if ray.IsKeyPressed(ray.KeyEnter) {
-
+			insertNote(textbox1.Text)
 			notes = append(notes, textbox1.Text)
 			textbox1.Text = ""
 		}
@@ -42,13 +114,13 @@ func main() {
 			ray.DrawText("Remove", 610, int32(yPosition), 20, ray.Black)
 			if ray.IsMouseButtonPressed(ray.MouseLeftButton) {
 				if ray.CheckCollisionPointRec(ray.GetMousePosition(), rbut) {
-          notes = notes[:len(notes)-1]
+          removeNote(i)
+					notes = notes[:len(notes)-1]
 					textbox1.Text = ""
 				}
 			}
 			yPosition += 50
 		}
-		ray.DrawText("Made using Raylib", 650, 550, 18, ray.LightGray)
 		ray.EndDrawing()
 	}
 
--- go.mod
+++ go.mod
@@ -2,10 +2,18 @@ module raytodo
 
 go 1.23.2
 
-require github.com/gen2brain/raylib-go/raylib v0.0.0-20241014163942-bf5ef1835077
+require (
+	github.com/gen2brain/raylib-go/raylib v0.0.0-20241014163942-bf5ef1835077
+	github.com/jackc/pgx/v5 v5.7.1
+)
 
 require (
 	github.com/ebitengine/purego v0.8.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
+	github.com/mattn/go-sqlite3 v1.14.24
+	golang.org/x/crypto v0.27.0 // indirect
 	golang.org/x/exp v0.0.0-20241009180824-f66d83c29e7c // indirect
 	golang.org/x/sys v0.26.0 // indirect
+	golang.org/x/text v0.18.0 // indirect
 )
